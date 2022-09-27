2db76f89cbdc7cde0b74937bea299e36a877ddbf
Kaleb Ugalde <kalebugalde@gmail.com>
2022-09-27 13:37:05 -0700

    Created first MATLAB script

---
+++ MATH 18/Intro2Matlab.m
@@ -0,0 +1,16 @@
+% Example 1.1 Basic operations
+disp(100^(3/2) + (5*7-99)/2)
+
+
+% Example 1.2 naming variables
+x = 5;
+l = 12; i = 9; n = 14; e = 5; a = 1; r = 18; g = 7; b = 2;
+DaysPerYear = 365;
+leapyear = DaysPerYear + 1;
+linearalgebra = l+i+n+e+a+r+a+l+g+e+b+r+a;
+
+disp(x)
+disp(DaysPerYear)
+disp(leapyear)
+disp(linearalgebra)
+
