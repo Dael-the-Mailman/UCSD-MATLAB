c93022ca6277393a54e07f6940e2c571040ec5c5
Kaleb Ugalde <kalebugalde@gmail.com>
2022-09-27 20:39:05 -0700

    Finished Intro to MATLAB

--- MATH 18/Intro2Matlab.m
+++ MATH 18/Intro2Matlab.m
@@ -14,3 +14,37 @@ disp(DaysPerYear)
 disp(leapyear)
 disp(linearalgebra)
 
+% 1.3 Hints and Tips
+
+m = -3:0.4:12; % Range of values
+n = 4:2:200; % Range of values
+
+% 1.4 Computer Algebra
+syms x y % Allows symbolic math
+a = (x+y)^3; 
+expand(a)
+factor(x^3 + 3*x^2*y + 3*x*y^2 + y^3)
+diff(sin(x)) % Matlab allows for derivatives(diff)
+                   % and integrals(int)
+diff(sin(x+3*y)) % Will default to take derivative with
+                 % respect to x
+diff(sin(x+3*y),y) % Derivative with respect to y
+
+% 1.5 Built-in Help
+% use the doc or help command for help with functions
+
+% 1.6 Matrices in MATLAB
+A = [2 1; 4 3];
+disp(A)
+A = [2,1; 4,3];
+disp(A) % Both definitions do the same thing
+
+% 1.7 Matrix Operations and Manipulation
+A(1,2) % Different ways to access matrix values
+A(2,:)
+
+% 1.8 Random Matrices
+RMatrix1 = rand(2) % Random 2x2 matrix
+
+
+
